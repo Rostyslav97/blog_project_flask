19d97c82e68cb10d1cbd6c5166f9586c64c0f8ce
Rostyslav <malanyukrostik@gmail.com>
2022-03-29 15:43:28 +0300

    add databases, templates, css

--- flsite.py
+++ flsite.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g 
 
 
 # Конфигурация WSGI приложения
@@ -9,8 +9,52 @@ DEBUG = True
 SECRET_KEY = 'sdg5cf2gh9sdf3se4f34'
 
 
+menu = [{"name": "Installation", "url": "install"},
+        {"name": "First App", "url": "first_app"},
+        {"name": "Contact", "url": "contact"}]
+
+
 app = Flask(__name__)
-app.config.from_object(__name__) # загружаем конфигурацию WSGI приложения
+app.config.from_object(__name__) 
 
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db'))) # переопределим путь к базе данных
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db() 
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read()) 
+    db.commit() 
+    db.close()
+
+
+def get_db():
+    """Соединение с БД, если оно еще не установлено"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db() 
+    return render_template('index.html', menu=menu) 
+
+
+@app.teardown_appcontext 
+def close_db(error): 
+    """Закрываем соединение с БД, если оно было установлено"""
+    if hasattr(g, 'link_db'): 
+        g.link_db.close() 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
